a03621bf4972df3d69ba473dcab59f87edec36bf
Yuvraj Choudhary <79357332+Yuvraj-choudhary@users.noreply.github.com>
2022-03-09 21:08:19 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,10 +2,10 @@ import firebase from "firebase/app";
 import 'firebase/auth'
 
 export const auth = firebase.initializeApp ({
-  apiKey: "AIzaSyA8z93GFGnBeGeYSUD3WTQ1G2fI6TXAyf8",
-  authDomain: "messenger-62072.firebaseapp.com",
-  projectId: "messenger-62072",
-  storageBucket: "messenger-62072.appspot.com",
-  messagingSenderId: "1008642704759",
-  appId: "1:1008642704759:web:63a966e02f58fae58d4d72",
+authDomain: "messenger-36860.firebaseapp.com",
+  projectId: "messenger-36860",
+  storageBucket: "messenger-36860.appspot.com",
+  messagingSenderId: "80849921731",
+  appId: "1:80849921731:web:b40418d9cdedee6215e476",
+  measurementId: "G-FKJD505F05"
 }).auth()
